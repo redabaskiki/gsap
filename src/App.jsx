a5d8b159ca5a4e4ad627c1e3d9ec30c0440e514a
reda baskiki <baskikireda.rb@gmail.com>
2025-09-15 19:06:42 +0200

    hero and cocktails

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from "gsap/all";
 
+
 import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Cocktails from './components/Cocktails.jsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
-const App = () => {
- return (
+ const App = () => {
 	<main>
 	 <Navbar />
+	 <Hero />
+	 <Cocktails />
 	</main>
- )
+ 
 }
 
 export default App
